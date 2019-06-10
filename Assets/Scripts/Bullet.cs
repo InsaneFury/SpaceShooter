7ad909187a79d2b3928af649bf48df165009e9f0
insanefury <justdarsan@gmail.com>
2019-06-10 03:40:51 -0300

    2942

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float timeToDisable = 3f;
+
+    private void OnEnable()
+    {
+        Invoke("DisableBullet", timeToDisable);
+    }
+
+    void DisableBullet()
+    {
+        gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/ParallaxFX.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxFX : MonoBehaviour
+{
+    public Vector2 speed;
+    MeshRenderer rend;
+    Material m;
+
+    void Start()
+    {
+        rend = GetComponent<MeshRenderer>();
+        m = rend.material;
+    }
+
+    void Update()
+    {
+        Parallax(m);
+    }
+
+    void Parallax(Material mat)
+    {
+        Vector2 offset = mat.mainTextureOffset;
+        offset += speed * Time.deltaTime;
+        mat.mainTextureOffset = offset;
+    }
+}
